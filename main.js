02c54dba663da6b6d849811ac1da2edba87ce97c
chinmayimaddali10 <71767205+chinmayimaddali10@users.noreply.github.com>
2021-06-20 09:26:24 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 var song = "";
 var song1 = "";
+leftWX = 0;
+leftWY = 0;
+rightWY = 0;
+rightWX = 0;
 
 function preload() {
     song = loadSound("Imagine Dragons - Believer.mp3");
     song1 = loadSound("Music.mp3");
-
+    song.setVolume(0.5);
+    song.rate(1);
 }
 
 function setup() {
@@ -12,8 +17,25 @@ function setup() {
     canvas.center();
     video = createCapture(450, 450);
     video.hide();
+    posenet = ml5.poseNet(video, modelLoaded);
+    posenet.on('pose', gotposes);
 }
 
 function draw() {
     image(video, 25, 25, 450, 450);
+}
+
+function modelLoaded() {
+    console.log("posenet is working");
+}
+
+function gotposes(results) {
+    if (results.length > 0) {
+        console.log(results);
+    }
+    leftWX = results[0].pose.leftWrist.x;
+    leftWY = results[0].pose.leftWrist.y;
+    rightWY = results[0].pose.rightWrist.y;
+    rightWX = results[0].pose.rightWrist.x;
+    console.log(" left wrist x = " + leftWX + " left wrist y = " + leftWY + " right wrist y = " + rightWY + " right wrist x = " + rightWX);
 }
